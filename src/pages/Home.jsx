a07d4d1cebef4c4a90c586c251b7614293eac2a1
Muhammad Mahendra Subrata <mumahendras3@gmail.com>
2023-05-01 14:53:49 +0700

    styles: make texts on home not the same width divider

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,16 +5,16 @@ import Col from 'react-bootstrap/Col';
 export default function Home() {
   return (
     <Container className="px-4 py-5">
-      <Row>
-        <Col xl className="d-flex flex-column justify-content-center align-items-center">
+      <Row className="justify-content-center">
+        <Col xs="11" lg className="d-flex flex-column justify-content-center align-items-center">
           <h1 className="text-white headline text-center">Hello there!</h1>
           <h5 className="text-white headline text-center mb-0">I'm mumahendras3, an Open Source and Linux enthusiast.</h5>
         </Col>
-        <Col xl="auto">
-          <div className="bg-white divider d-xl-none"></div>
-          <div className="bg-white divider-xl d-none d-xl-block"></div>
+        <Col lg="auto">
+          <div className="bg-white divider d-lg-none"></div>
+          <div className="bg-white divider-lg d-none d-lg-block"></div>
         </Col>
-        <Col xl className="d-flex justify-content-center align-items-center">
+        <Col xs="11" lg className="d-flex justify-content-center align-items-center">
           <p className="text-white short-profile mb-0">
             I have a bachelor’s degree in Engineering Physics from Faculty of Industrial Technology, Institut Teknologi Bandung (ITB). I am interested in Unix-like systems administration, shell scripting, and programming.
           </p>
